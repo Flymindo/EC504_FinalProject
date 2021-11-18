73e4c996cd7e3ddaef3898191acb803782fd78dd
unknown <Charles Mo>
2021-11-17 19:18:57 -0500

    added propsal and implementation slides

--- watershed.py
+++ watershed.py
@@ -7,6 +7,8 @@ copy = np.copy(img)
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
 
+cv.imwrite("images/coins_otsus.png", thresh)
+
 # noise removal
 kernel = np.ones((3,3),np.uint8)
 opening = cv.morphologyEx(thresh,cv.MORPH_CLOSE,kernel, iterations = 5)
