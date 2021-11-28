e0568ff8ae7550ad05bcefbd58fbbfb9e5cab8e2
Flymindo <kaahsh386@gmail.com>
2021-11-28 18:20:32 -0500

    background foreground probability save to textfile

--- gaussian.py
+++ gaussian.py
@@ -3,21 +3,33 @@ import matplotlib.pyplot as plt
 import cv2
 from sklearn.mixture import GaussianMixture as GMM
  
+
 image = cv2.imread('images/coins.jpg')
+#print(image.shape)
+
+# Get rid of BGR to GRAY scale conversion if image scale is already in grayscale
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 #print(gray)
-print(gray.shape[0])
+#print(gray.shape)
 np.savetxt('image.txt',gray )
 
-img = image.reshape(-1,3)
-print(img.shape)
 
-gmm_model = GMM(n_components = 2, covariance_type ='tied').fit(img)
+gray = gray.reshape(-1,1)
+
+gmm_model = GMM(n_components = 2, covariance_type ='tied').fit(gray)
 
 
-segmented_data = gmm_model.predict_proba(img)
+segmented_data = gmm_model.predict_proba(gray)
 
 # we can assign edge weights with prob = gmm_model.predict_proba(img), prob[:,0] => foreground to nodes, prob[:,1] => background to nodes
 
 print(segmented_data.shape)
-print(segmented_data[:,0])
+
+foreground = segmented_data[:,0]
+background = segmented_data[:,1]
+foreground = foreground.reshape(240,180)
+background = background.reshape(240,180)
+
+np.savetxt('foreground.txt',foreground)
+np.savetxt('background.txt',background)
+
