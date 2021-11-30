e46e594eae217c73698c61add3e092acc2c72923
Flymindo <kaahsh386@gmail.com>
2021-11-30 00:19:03 -0500

    add

--- graph.cpp
+++ graph.cpp
@@ -10,8 +10,14 @@ struct node{
    struct arc *first; /* first arc in linked list */
    int D;  /* Distance estimate */
    int P;  /* Predecessor node in shortest path */
-   int Q;  /* Position of node in heap, from 1 to Nm, where 1 is best */
+   float pixel;  /* Position of node in heap, from 1 to Nm, where 1 is best */
    };
+
+struct arc{
+  struct arc *next;
+  float capacity;
+  int end;
+  };
 
 
 int height;
@@ -22,6 +28,9 @@ int main(int argc, char *argv[]){
     
     ifstream inFile;
     float data;
+    struct arc *edge, test;
+    struct node s,t;
+    
     
     
     inFile.open("imageSize.txt");
@@ -41,11 +50,9 @@ int main(int argc, char *argv[]){
         Graph[count_row][count_col].first = NULL;
         Graph[count_row][count_col].D = count_row;
         Graph[count_row][count_col].P = count_col;
-        Graph[count_row][count_col].Q = 0;
+        Graph[count_row][count_col].pixel = data;
         
         count_col++;
-//        cout << count_row << endl;
-        
         if (count_col == width){
             count_col = 0;
             count_row +=1;
@@ -53,15 +60,31 @@ int main(int argc, char *argv[]){
     
         
     }
+//    cout << Graph[0][1].pixel << endl;
+    inFile.close();
+    
+    inFile.open("foreground.txt");
+    // 0 => foreground
+    count_col=0;
+    count_row=0;
+    while(inFile >> data){
+        edge = (struct arc *)malloc(sizeof(test));
+        edge->capacity = data; edge->end = 0;
+        edge->next = Graph[count_row][count_col].first;
+        Graph[count_row][count_col].first=edge;
+        
+        count_col++;
+        if (count_col == width){
+            count_col = 0;
+            count_row +=1;
+        }
+    }
+    cout << Graph[0][1].first->capacity << endl;
+    
     inFile.close();
     
     
-    cout << Graph[2][55].D << "    " << Graph[2][55].P << endl;
-    cout << Graph[23][10].D << "   "  << Graph[23][10].P << endl;
-    cout << Graph[239][179].D << "   "  << Graph[239][179].P << endl;
     
-    
-
   
 
 }
