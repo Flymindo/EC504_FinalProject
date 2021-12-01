1cd1e15004f8c7ee29d224c0b4d89f2ace3cabba
Flymindo <kaahsh386@gmail.com>
2021-12-01 14:53:30 -0500

    Adding edges on the graph

--- graph.cpp
+++ graph.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
 
 #define maxnodes 200000
+#define SIGMA 30
+#define SQUARE(X) X*X
 
 struct node{
    struct arc *first; /* first arc in linked list */
    int D;  /* Distance estimate */
    int P;  /* Predecessor node in shortest path */
-   float pixel;  /* Position of node in heap, from 1 to Nm, where 1 is best */
+   double pixel;  /* Position of node in heap, from 1 to Nm, where 1 is best */
    };
 
 struct arc{
   struct arc *next;
-  float capacity;
-  int end;
+  double capacity;
+  int end [2];
   };
 
 
-int height;
-int width;
+double penalty(double a, double b);
+
+
+
 
 
 int main(int argc, char *argv[]){
     
     ifstream inFile;
-    float data;
+    double data;
     struct arc *edge, test;
     struct node s,t;
+    
+    int height;
+    int width;
     
     
     
@@ -60,31 +68,75 @@ int main(int argc, char *argv[]){
     
         
     }
-//    cout << Graph[0][1].pixel << endl;
     inFile.close();
     
-    inFile.open("foreground.txt");
-    // 0 => foreground
-    count_col=0;
-    count_row=0;
-    while(inFile >> data){
-        edge = (struct arc *)malloc(sizeof(test));
-        edge->capacity = data; edge->end = 0;
-        edge->next = Graph[count_row][count_col].first;
-        Graph[count_row][count_col].first=edge;
-        
-        count_col++;
-        if (count_col == width){
-            count_col = 0;
-            count_row +=1;
+    double bp;
+    
+    for (int i=0; i< height; i++){
+        for (int j=0; j<width; j++){
+            if (i-1 >= 0){
+                bp = penalty(Graph[i][j].pixel, Graph[i-1][j].pixel);
+                edge = (struct arc *)malloc(sizeof(test));
+                edge->capacity = bp; edge->end[0] = i-1; edge->end[1] =j;
+                edge->next = Graph[i][j].first;
+                Graph[i][j].first=edge;
+            }
+            if (i+1 < height){
+                bp = penalty(Graph[i][j].pixel, Graph[i+1][j].pixel);
+                edge = (struct arc *)malloc(sizeof(test));
+                edge->capacity = bp; edge->end[0] = i+1; edge->end[1] = j;
+                edge->next = Graph[i][j].first;
+                Graph[i][j].first=edge;
+            }
+            if (j-1 >= 0){
+                bp = penalty(Graph[i][j].pixel, Graph[i][j-1].pixel);
+                edge = (struct arc *)malloc(sizeof(test));
+                edge->capacity = bp; edge->end[0]= i; edge->end[1] = j-1;
+                edge->next = Graph[i][j].first;
+                Graph[i][j].first=edge;
+            }
+            if (j+1 < width){
+                bp = penalty(Graph[i][j].pixel, Graph[i][j+1].pixel);
+                edge = (struct arc *)malloc(sizeof(test));
+                edge->capacity = bp; edge->end[0] = i; edge->end[1] = j+1;
+                edge->next = Graph[i][j].first;
+                Graph[i][j].first=edge;
+            }
         }
     }
-    cout << Graph[0][1].first->capacity << endl;
     
-    inFile.close();
+    while(Graph[1][1].first){
+        cout << Graph[1][1].first->end[0] << "   ";
+        cout << Graph[1][1].first->end[1] << "   " << Graph[1][1].first->capacity << endl;
+        Graph[1][1].first = Graph[1][1].first->next;
+    }
+    
+//    inFile.open("foreground.txt");
+//    // 0 => foreground
+//    count_col=0;
+//    count_row=0;
+//    while(inFile >> data){
+//        edge = (struct arc *)malloc(sizeof(test));
+//        edge->capacity = data; edge->end = 0;
+//        edge->next = Graph[count_row][count_col].first;
+//        Graph[count_row][count_col].first=edge;
+//
+//        count_col++;
+//        if (count_col == width){
+//            count_col = 0;
+//            count_row +=1;
+//        }
+//    }
+//    cout << Graph[0][1].first->capacity << endl;
+//
+//    inFile.close();
     
     
-    
-  
 
+}
+
+double penalty(double a, double b) {
+//    cout << 100 * exp(-SQUARE((double)a - (double)b)) / (2 * SQUARE(SIGMA)) << endl;
+//    return 100 * exp(-SQUARE((int)a - (int)b)) / (2 * SQUARE(SIGMA));
+    return 1- SQUARE( ((a-b)/255) );
 }
