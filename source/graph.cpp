45a0ba3c5938c4c9435a1e8e06f20b2b8c61743d
Charles Mo <mo.charles2019@gmail.com>
2021-12-10 14:03:16 -0500

    removed redundant struct def

--- source/graph.cpp
+++ source/graph.cpp
@@ -5,23 +5,6 @@ using namespace std;
 #define SIGMA 30
 #define SQUARE(X) ((X)*(X))
 
-struct node{
-    struct arc *first; /* first arc in linked list */
-    int flow;  /* Distance estimate */
-//    struct node *P;  /* Predecessor node in shortest path */
-    int curr[2];
-    int prev[2];  /* Predecessor node in shortest path */
-    struct arc *wentThrough;
-    double pixel;  /* Position of node in heap, from 1 to Nm, where 1 is best */
-};
-
-struct arc{
-    struct arc *next;
-    int capacity;
-    struct arc *neigh;
-    int end [2];
-};
-
 int bfs(node sNode,node Graph[][10000], int height, int width)
 {
     queue<node> q;//declare queue vector
